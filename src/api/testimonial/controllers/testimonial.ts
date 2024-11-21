5f939450c2bdb266a44b0c66bbf0f2971e0e942d
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-11-21 22:52:38 +0530

    added testimonial

--- src/api/testimonial/controllers/testimonial.ts
+++ src/api/testimonial/controllers/testimonial.ts
@@ -1,7 +1,22 @@
-/**
- * testimonial controller
- */
+// /**
+//  * testimonial controller
+//  */
 
-import { factories } from '@strapi/strapi'
+// import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::testimonial.testimonial');
+// export default factories.createCoreController('api::testimonial.testimonial');
+
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::testimonial.testimonial', ({ strapi }) => ({
+  async find(ctx) {
+    // Fetch all testimonials and include the Image field
+    const testimonials = await strapi.db.query('api::testimonial.testimonial').findMany({
+      populate: ['Image'], // Populating the Image field to include media details
+    });
+
+    // Return the fetched testimonials with image details
+    return testimonials;
+  },
+}));
