b7c9724d7a28508d569f860e2368325f0c76b988
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-11-21 22:44:25 +0530

    added blog apis

--- src/api/blog/controllers/blog.ts
+++ src/api/blog/controllers/blog.ts
@@ -1,37 +1,7 @@
-// /**
-//  * blog controller
-//  */
+/**
+ * blog controller
+ */
 
-// import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi'
 
-// export default factories.createCoreController('api::blog.blog');
-
-import { factories } from "@strapi/strapi";
-
-export default factories.createCoreController("api::blog.blog", ({ strapi }) => ({
-  async find(ctx) {
-    // Use the core service to fetch all blog entries and populate the Image field
-    const blogs = await strapi.db.query("api::blog.blog").findMany({
-      populate: ["Image"], // Populate Image field
-    });
-
-    // Return the fetched blogs
-    return blogs;
-  },
-  async findOne(ctx) {
-    const { id } = ctx.params;
-
-    // Custom logic: Fetch the blog entry
-    const blogEntry = await strapi.db.query("api::blog.blog").findOne({
-      where: { id },
-      populate: ["Image"], // Populate related fields as needed
-    });
-
-    if (!blogEntry) {
-      return ctx.notFound("Blog entry not found");
-    }
-
-    // Return the blog entry
-    return blogEntry;
-  },
-}));
+export default factories.createCoreController('api::blog.blog');
